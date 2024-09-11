257bd8551044aa85f92f9991a870d74b0eb0d5c4
Rudy Nurafif <rudynurafif@gmail.com>
2024-09-12 05:33:43 +0700

    v-if directive

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script>
+export default {
+  data() {
+    return {
+      name: 'John Doe',
+      status: 'pending',
+    };
+  },
+};
+</script>
+
+<template>
+  <h1>{{ name }}</h1>
+  <p v-if="status === 'active'">User is active</p>
+  <p v-else-if="status === 'pending'">User is pending</p>
+  <p v-else>User is inactive</p>
+</template>
