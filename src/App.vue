74d1c5d35f40c1c4b9fd3ee6a061cc09f496bf9c
Rudy Nurafif <rudynurafif@gmail.com>
2024-09-12 05:38:23 +0700

    v-for directive

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,7 @@ export default {
     return {
       name: 'John Doe',
       status: 'pending',
+      tasks: ['Task One', 'Task Two', 'Task Three'],
     };
   },
 };
@@ -11,7 +12,17 @@ export default {
 
 <template>
   <h1>{{ name }}</h1>
+
+  <br>
+
   <p v-if="status === 'active'">User is active</p>
   <p v-else-if="status === 'pending'">User is pending</p>
   <p v-else>User is inactive</p>
+
+  <br>
+
+  <h3>Task:</h3>
+  <ul>
+    <li v-for="task in tasks" :key="task">{{ task }}</li>
+  </ul>
 </template>
