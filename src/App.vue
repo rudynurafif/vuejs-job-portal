3fdbf0e9890d5e7a8fc2381d255559eaac5fb2f7
Rudy Nurafif <rudynurafif@gmail.com>
2024-09-12 06:51:32 +0700

    Tailwind setup and navbar component

--- src/App.vue
+++ src/App.vue
@@ -1,69 +1,7 @@
 <script setup>
-import { onMounted, ref } from 'vue';
-
-const name = ref('John Doe');
-const status = ref('active');
-const tasks = ref(['Task One', 'Task Two', 'Task Three']);
-const newTask = ref('');
-
-const toggleStatus = () => {
-  if (status.value === 'active') status.value = 'pending';
-  else if (status.value === 'pending') status.value = 'inactive';
-  else status.value = 'active';
-};
-
-const addTask = () => {
-  if (newTask.value.trim('') !== '') {
-    tasks.value.push(newTask.value);
-    newTask.value = '';
-  }
-};
-
-const deleteTask = (index) => {
-  tasks.value.splice(index, 1)
-}
-
-onMounted(async () => {
-  try {
-    const response = await fetch('https://jsonplaceholder.typicode.com/todos')
-    const data = await response.json()
-    tasks.value = data.map((task) => task.title)
-  } catch (error) {
-    console.log('Error fetching tasks')
-  }
-})
+import Navbar from './components/Navbar.vue';
 </script>
 
 <template>
-  <h1>{{ name }}</h1>
-
-  <br />
-
-  <p v-if="status === 'active'">User is active</p>
-  <p v-else-if="status === 'pending'">User is pending</p>
-  <p v-else>User is inactive</p>
-
-  <br />
-
-  <form @submit.prevent="addTask">
-    <label for="newTask">Add Task </label>
-    <input type="text" name="newTask" id="newTask" v-model="newTask" />
-    <button type="submit">Submit</button>
-  </form>
-
-  <br />
-
-  <h3>Task:</h3>
-  <br />
-  <ul>
-    <li v-for="(task, index) in tasks" :key="task">
-      <span>{{ task }}</span>
-      <button @click="deleteTask(index)">x</button>
-    </li>
-  </ul>
-
-  <br />
-
-  <!-- <button v-on:click="toggleStatus">Change Status</button> -->
-  <button @click="toggleStatus">Change Status</button>
+  <Navbar />
 </template>
