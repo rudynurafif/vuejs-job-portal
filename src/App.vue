83c003799babe416249832865dee894554e4b6df
Rudy Nurafif <rudynurafif@gmail.com>
2024-09-12 09:28:49 +0700

    Job Listing component

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,13 @@
 <script setup>
 import Hero from './components/Hero.vue';
+import HomeCards from './components/HomeCards.vue';
+import JobListings from './components/JobListings.vue';
 import Navbar from './components/Navbar.vue';
 </script>
 
 <template>
   <Navbar />
   <Hero />
+  <HomeCards />
+  <JobListings />
 </template>
